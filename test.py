2953d248b3b1e08326d06287b81772f5f915dccf
Vidix-JS <vadim.bakulin.1996@mail.ru>
2024-09-24 12:44:50 +0300

    добавление еще одной операции

--- test.py
+++ test.py
@@ -1,9 +1,13 @@
 print("Введите тип операции: ")
 str = input()
+print("Введите значение для первой переменной: ")
 a = int(input())
+print("Введите значение для второй переменной: ")
 b = int(input())
 if str == "+":
     print(a+b)
 elif str == "-":
     print(a-b)
+elif str == "/" or str == ":":
+    print(a / b)
 else: print("Выбрана не та операция")
